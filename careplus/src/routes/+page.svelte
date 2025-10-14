489150c4fb4457f77f12dda7f3e5ca0d568bb5f3
arnoldfromancius <arnoldfromancius@gmail.com>
2025-10-14 12:36:06 +0200

    building

--- careplus/src/routes/+page.svelte
+++ careplus/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <div class="heading1">
 <h1>Mighka CarePlus+</h1>
 </div>
+<div>
+    <img src="src/lib/assets/images/banner.jpg" alt="Mighka CarePlus+ banner"/>
+</div>
 <style lang="css">
     h1 {
         font-size: 2.0rem;
@@ -8,14 +11,22 @@
         color: #fce66b;
         margin-bottom: 1rem;
         text-align: center;
-        background-color: rgb(87, 59, 91);
+        background-color: rgb(24, 47, 44);
     }
+
+    img{
+        width: 100%;
+        height: auto;
+        display: block;
+        margin: 0 auto;
+    }
+
     .heading1 {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 4rem;
-        background-color: rgb(69, 48, 73);
+        background-color: rgb(7, 15, 14);
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     }
 </style>

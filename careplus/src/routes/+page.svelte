1701d5403a6a54233ab29625e4f1e0b8d5d5112a
arnoldfromancius <arnoldfromancius@gmail.com>
2025-10-14 12:16:59 +0200

    building

--- careplus/src/routes/+page.svelte
+++ careplus/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="heading1">
+<h1>Mighka CarePlus+</h1>
+</div>
+<style lang="css">
+    h1 {
+        font-size: 2.0rem;
+        font-weight: bold;
+        color: #fce66b;
+        margin-bottom: 1rem;
+        text-align: center;
+        background-color: rgb(87, 59, 91);
+    }
+    .heading1 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 4rem;
+        background-color: rgb(69, 48, 73);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+</style>
